e6e98120b98b5f6e779bb4437e773bf953500a22
reproducepark <reproducepark@kaist.ac.kr>
2024-05-11 14:46:22 +0900

    step function 으로 변경 필요

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
-import { StatusBar } from "expo-status-bar";
-import { Image, ImageBackground, StyleSheet, View, Text } from "react-native";
+import React, { useEffect } from "react";
+import { Image, ImageBackground, StyleSheet, View, Animated, Easing } from "react-native";
 
 const BackGroundImage = require("./assets/images/img_bg_mymnd.png");
 const CameraImg = require("./assets/images/img_policy_state_camera_block.png");
-
+const CircleImg = require("./assets/images/circle_image.png");
 export default function App() {
+  const rotation = new Animated.Value(0);
+
+  useEffect(() => {
+    Animated.loop(
+      Animated.timing(rotation, {
+        toValue: 1,
+        duration: 2500,
+        easing: Easing.linear,
+        useNativeDriver: true,
+      })
+    ).start();
+  }, [rotation]);
+
+  const spin = rotation.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['0deg', '360deg'],
+  });
+
   return (
     <View style={styles.container}>
       <ImageBackground
@@ -16,6 +34,10 @@ export default function App() {
             source={CameraImg}
             style={styles.camera}>
           </Image>
+          <Animated.Image
+            source={CircleImg}
+            style={[styles.circle, { transform: [{ rotate: spin }] }]}>
+          </Animated.Image>
         </View>
       </ImageBackground>
     </View>
@@ -28,21 +50,27 @@ const styles = StyleSheet.create({
     backgroundColor: '#212121',
   },
   overlay: {
-    flexDirection: 'row', // 수평 방향으로 정렬하기 위해 설정
-    justifyContent: 'center', // 수평 중앙 정렬
-    alignItems: 'center', // 수직 중앙 정렬
-    position: 'absolute', // overlay를 위치시킬 때 절대 위치로 설정
-    bottom: 250, // 아래로 20만큼 떨어지도록 설정
-    left: 0, // 왼쪽 정렬
-    right: 0, // 오른쪽 정렬
+    position: 'absolute',
+    top: '50%', // 배경 이미지의 상단에서 중앙으로 이동
+    left: '50%', // 배경 이미지의 왼쪽에서 중앙으로 이동
   },
   camera: {
-    // position: 'absolute', // overlay를 위치시킬 때 절대 위치로 설정
-    // bottom: 150, // 아래로 20만큼 떨어지도록 설정
-    width: 200, // 이미지의 너비
-    height: 200, // 이미지의 높이
+    width: 185, // 이미지의 너비
+    height: 185, // 이미지의 높이
     borderRadius: 100, // 원형 모양을 위한 반지름
     resizeMode: 'cover', // 이미지를 cover로 설정하여 비율 유지
+    marginLeft: -(185/2), // 이미지의 가로 크기 절반만큼 왼쪽으로 이동
+    marginTop: -17, // 이미지의 세로 크기 절반만큼 위로 이동
   },
+  circle: {
+    width: 244,
+    height: 244,
+    position: 'absolute',
+
+    marginLeft: -122, // 이미지의 가로 크기 절반만큼 왼쪽으로 이동
+    marginTop: -47, // 이미지의 세로 크기 절반만큼 위로 이동
+
+  },
+
 
 });
