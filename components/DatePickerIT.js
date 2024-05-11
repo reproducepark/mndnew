7e0099cb65e54734cf39297a39c170546f6bcf61
reproducepark <reproducepark@kaist.ac.kr>
2024-05-11 20:14:57 +0900

    date와 time 분리

--- components/DatePickerIT.js
+++ components/DatePickerIT.js
@@ -2,34 +2,62 @@ import React, { useState } from 'react';
 import { View, TouchableOpacity, Text, StyleSheet } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 
-const DateTimePickerForIT = ({setInstalledDate}) => {
-    const [dateTime, setDateTime] = useState(new Date());
-    const [showDateTimePicker, setShowDateTimePicker] = useState(false);
+const DateTimePickerForIT = ({ setInstalledDate }) => {
+    const [date, setDate] = useState(new Date());
+    const [showDatePicker, setShowDatePicker] = useState(false);
+    const [showTimePicker, setShowTimePicker] = useState(false);
 
-    const handleDateTimeChange = (event, selectedDateTime) => {
-        const currentDateTime = selectedDateTime || dateTime;
-        setShowDateTimePicker();
-        setDateTime(currentDateTime);
-        setInstalledDate(currentDateTime);
-        console.log(currentDateTime);
+    const handleDateChange = (event, selectedDate) => {
+        const currentDate = selectedDate || date;
+        setShowDatePicker();
+        setDate(currentDate);
+        setInstalledDate(currentDate);
+        console.log(currentDate);
     };
 
-    const showDateTimePickerModal = () => {
-        setShowDateTimePicker(true);
+    const handleTimeChange = (event, selectedTime) => {
+        const currentDate = selectedTime || date;
+        setShowTimePicker(); // iOS에서는 onChange에서만 시간을 닫습니다.
+        // 시간을 선택한 뒤에 할 작업을 여기에 추가할 수 있습니다.
+        setShowDatePicker();
+        setDate(currentDate);
+        setInstalledDate(currentDate);
+        console.log(currentDate);
     };
+
+    const showDatePickerModal = () => {
+        setShowDatePicker(true);
+    };
+
+    const showTimePickerModal = () => {
+        setShowTimePicker(true);
+    };
+
 
     return (
         <View style={styles.container}>
-            <TouchableOpacity onPress={showDateTimePickerModal} style={styles.touchArea}>
-                <Text style={styles.touchText}>           </Text>
+            <TouchableOpacity onPress={showDatePickerModal} style={styles.touchArea}>
+                <Text style={styles.touchText}>날짜 선택</Text>
             </TouchableOpacity>
-            {showDateTimePicker && (
+            {showDatePicker && (
                 <DateTimePicker
-                    value={dateTime}
-                    mode="datetime"
-                    is24Hour="true"
+                    value={date}
+                    mode="date"
                     display="default"
-                    onChange={handleDateTimeChange}
+                    onChange={handleDateChange}
+                />
+            )}
+
+            <TouchableOpacity onPress={showTimePickerModal} style={styles.touchArea}>
+                <Text style={styles.touchText}>시간 선택</Text>
+            </TouchableOpacity>
+            {showTimePicker && (
+                <DateTimePicker
+                    value={date}
+                    mode="time"
+                    is24Hour={true}
+                    display="default"
+                    onChange={handleTimeChange}
                 />
             )}
         </View>
